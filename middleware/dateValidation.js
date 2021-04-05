77c896d45ba62d1d8b226f3d1d0f2cc26677a4a1
zcouts <zcouts@ltu.edu>
2021-04-05 15:37:46 -0400

    Updated dateValidation

--- middleware/dateValidation.js
+++ middleware/dateValidation.js
@@ -1,5 +1,15 @@
-const StatusCode = require('http-status-codes');
+const StatusCodes = require('http-status-codes');
 
 module.exports = (req, res, next) => {
-    
+    const headerDate = req.header['date-validation'];
+    const queryDate = req.query['date-validation'];
+    const epochTime = Math.round(Date.now() / 1000);
+
+    if(headerDate && queryDate === false){
+        res.status(StatusCodes.UNAUTHORIZED).send('Both headerDate and queryDate are invalid')
+    } else if(headerDate === false || queryDate === false){
+        res.status(StatusCodes.UNAUTHORIZED).send('Either headerDate or queryDate are invalid')
+    }
+
+   next();
 };
