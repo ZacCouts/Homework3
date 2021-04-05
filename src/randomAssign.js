3ca74caeb56062daf988b4cece93ca8e945994c3
zcouts <zcouts@ltu.edu>
2021-04-05 13:42:53 -0400

    Update

--- src/randomAssign.js
+++ src/randomAssign.js
@@ -2,10 +2,10 @@ const Router = require('express').Router();
 const StatusCodes = require('http-status-codes');
 
 Router.all('/', (req, res) => {
-   if(Math.floor(Math.random() * 100) + 1 > 50){
-       response.status(StatusCodes.OK).send('Hello World');
+   if(Math.floor(Math.random() * 100) > 50){
+       res.status(StatusCodes.OK).send('Hello World');
    } else{
-       throw new Error('OOps');
+       throw new Error('Oops');
    }
 });
 
--- src/server.js
+++ src/server.js
@@ -6,10 +6,13 @@ const NoDelete = require('../middleware/noDelete');
 
 const Winston = require('../middleware/winston');
 
+const Router = require('../src/randomAssign');
+
 const app = Express();
 
 app.use(BodyParser.json());
 app.use(NoDelete);
+app.use(Router);
 app.use(Winston);
 
 app.listen(8080);
