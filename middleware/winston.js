7ee5e9d1dfbff27db608954c93ca3f931224228b
zcouts <zcouts@ltu.edu>
2021-04-05 12:49:05 -0400

    added winston

--- middleware/winston.js
+++ middleware/winston.js
@@ -1,16 +1,24 @@
 const winston = require('winston');
+const dateValid = require('dateValidation');
 
 const logger = winston.createLogger({
     level:'info',
     defaultMeta: { service: 'user-service' },
     transports: [
         new winston.transports.Console({
-            format: winston.format.json(),
+            format: winston.format.simple(),
         })
     ]
 });
 
 module.exports = (req, res, next) =>{
-  logger.info('Current server time (epoch): ${Date.now() / 1000}');
+  logger.info('Current server time (epoch): ', {Date:now() / 1000});
+  logger.info('HTTP verb: ', req.method);
+  logger.info('URL: ', req.url);
+  logger.info('Body: ', req.body);
+  logger.info('Query Parameters: ', req.query);
+  logger.info('Headers: ', req.headers);
+  logger.info('Date Validation: ', req.dateValid);
+  next();
 
 };
--- src/server.js
+++ src/server.js
@@ -4,9 +4,12 @@ const BodyParser = require('body-parser');
 
 const NoDelete = require('../middleware/noDelete');
 
+const Winston = require('../middleware/winston');
+
 const app = Express();
 
 app.use(BodyParser.json());
 app.use(NoDelete);
+app.use(Winston);
 
 app.listen(8080);
