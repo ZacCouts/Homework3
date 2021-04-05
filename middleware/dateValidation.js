219572621507173967361adc1cd8516dbdb85e01
zcouts <zcouts@ltu.edu>
2021-04-05 16:01:03 -0400

    Finished up Date Validation
    Still confused on how to get it to work but the idea is there

--- middleware/dateValidation.js
+++ middleware/dateValidation.js
@@ -3,13 +3,30 @@ const StatusCodes = require('http-status-codes');
 module.exports = (req, res, next) => {
     const headerDate = req.header['date-validation'];
     const queryDate = req.query['date-validation'];
-    const epochTime = Math.round(Date.now() / 1000);
+    const timeConvert = Math.round(Date.now() / 1000);
 
     if(headerDate && queryDate === false){
         res.status(StatusCodes.UNAUTHORIZED).send('Both headerDate and queryDate are invalid')
     } else if(headerDate === false || queryDate === false){
         res.status(StatusCodes.UNAUTHORIZED).send('Either headerDate or queryDate are invalid')
-    }
+    } else{
+        Object.keys(headerDate).forEach((key) => {
+            headerDate[key] = Number.parseInt(headerDate, 10);
+        });
+        Object.keys(queryDate).forEach((key) => {
+            queryDate[key] = Number.parseInt(queryDate, 10);
+        });
+        const EpochHeader = headerDate;
+        const EpochQuery = queryDate;
 
+        if(EpochHeader > (timeConvert- 300) && EpochHeader < (timeConvert + 300)) {
+            req.dateValidation = EpochHeader;
+            req.timeConvert = timeConvert;
+        }
+        if(EpochQuery > (timeConvert- 300) && EpochQuery < (timeConvert + 300)) {
+            req.dateValidation = EpochQuery;
+            req.timeConvert = timeConvert;
+        }
+    }
    next();
 };
