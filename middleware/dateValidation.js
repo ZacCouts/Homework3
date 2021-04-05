81eb7f7b342b147612b98d52b0d28c4eaaaceafa
zcouts <zcouts@ltu.edu>
2021-04-05 16:08:37 -0400

    Update dateValidation.js

--- middleware/dateValidation.js
+++ middleware/dateValidation.js
@@ -16,6 +16,7 @@ module.exports = (req, res, next) => {
         Object.keys(queryDate).forEach((key) => {
             queryDate[key] = Number.parseInt(queryDate, 10);
         });
+
         const EpochHeader = headerDate;
         const EpochQuery = queryDate;
 
